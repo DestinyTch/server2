a7266fc83a0ed5b00a70c4e2e5b6595821782349
Destiny Ezekiel <DestinyTch@proton.me>
2025-05-09 13:42:59 +0100

    Update server2.py

--- server2.py
+++ server2.py
@@ -61,4 +61,4 @@ def admin_login():
 if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 5001))
-    app.run(host='0.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
